4c3d403fc6203fcbd35c84bfa8c09a9335083fcd
Christopher Davis <cdavis9999@gmail.com>
2024-12-31 18:19:09 -0600

    Add Tests for PKCE

---
+++ pkce_test.go
@@ -0,0 +1,126 @@
+package oauth2server_test
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/chrisguitarguy/oauth2server"
+)
+
+func TestDefaultPKCE_SupportsKnownMethods(t *testing.T) {
+	pk := oauth2server.NewDefaultPKCE()
+
+	for _, method := range []string{oauth2server.CodeChallengeMethodPlain, oauth2server.CodeChallengeMethodS256} {
+		t.Run(method, func(t *testing.T) {
+			supports := pk.SupportsChallengeMethod(method)
+
+			if !supports {
+				t.Errorf("expected default pkce to support %q", method)
+			}
+		})
+	}
+}
+
+func TestDefaultPKCE_DoesNotSupportUnknownMethods(t *testing.T) {
+	pk := oauth2server.NewDefaultPKCE()
+
+	supports := pk.SupportsChallengeMethod("something")
+
+	if supports {
+		t.Error("did not expect default PKCE to support random challenge methods")
+	}
+}
+
+func TestDefaultPKCE_VerifyCodeChallenge_ErrorsIfInvalidChallengeMethodIsPassed(t *testing.T) {
+	pk := oauth2server.NewDefaultPKCE()
+
+	_, err := pk.VerifyCodeChallenge(context.Background(), "badmethod", "challenge", "verifier")
+
+	if err == nil {
+		t.Fatal("expected error from unknown method")
+	}
+	if !errors.Is(err, oauth2server.ErrUnsupportedCodeChallengeMethod) {
+		t.Errorf("expected ErrUnsupportedCodeChallengeMethod, got %v", err)
+	}
+}
+
+func TestDefaultPKCE_VerifyCodeChallenge_WithPlainValidatesIfVerififierMatches(t *testing.T) {
+	pk := oauth2server.NewDefaultPKCE()
+	challenge := "challenge"
+	verifier := challenge
+
+	ok, err := pk.VerifyCodeChallenge(
+		context.Background(),
+		oauth2server.CodeChallengeMethodPlain,
+		challenge,
+		verifier,
+	)
+
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+	if !ok {
+		t.Error("expected challenge to be verified")
+	}
+}
+
+func TestDefaultPKCE_VerifyCodeChallenge_WithPlainDoesNotValidateIfVerifierDoesNotMatch(t *testing.T) {
+	pk := oauth2server.NewDefaultPKCE()
+	challenge := "challenge"
+	verifier := "not correct"
+
+	ok, err := pk.VerifyCodeChallenge(
+		context.Background(),
+		oauth2server.CodeChallengeMethodPlain,
+		challenge,
+		verifier,
+	)
+
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+	if ok {
+		t.Error("expected challenge would not be verified")
+	}
+}
+
+func TestDefaultPKCE_VerifyCodeChallenge_WithS256ValidatesIfVerifierMatches(t *testing.T) {
+	pk := oauth2server.NewDefaultPKCE()
+	challenge := "n4bQgYhMfWWaL-qgxVrQFaO_TxsrC4Is0V1sFbDwCgg="
+	verifier := "test"
+
+	ok, err := pk.VerifyCodeChallenge(
+		context.Background(),
+		oauth2server.CodeChallengeMethodS256,
+		challenge,
+		verifier,
+	)
+
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+	if !ok {
+		t.Error("expected challenge to be verified")
+	}
+}
+
+func TestDefaultPKCE_VerifyCodeChallenge_WithS256DoesNotValidateIfVerifierDoesNotMatch(t *testing.T) {
+	pk := oauth2server.NewDefaultPKCE()
+	challenge := "n4bQgYhMfWWaL-qgxVrQFaO_TxsrC4Is0V1sFbDwCgg="
+	verifier := "notcorrect"
+
+	ok, err := pk.VerifyCodeChallenge(
+		context.Background(),
+		oauth2server.CodeChallengeMethodS256,
+		challenge,
+		verifier,
+	)
+
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+	if ok {
+		t.Error("expected challenge not to be verified")
+	}
+}
